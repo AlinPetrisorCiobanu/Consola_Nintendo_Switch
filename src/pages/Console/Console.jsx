01df8f40aba52faee2471a3e7ac0a811b6f53355
Rumanito12 <ady_9697@yahoo.com>
2024-04-03 11:54:34 +0200

    UPDATE: se añade metodo para dispositivos moviles en horizontal.

--- src/pages/Console/Console.jsx
+++ src/pages/Console/Console.jsx
@@ -2,30 +2,53 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import "./Console.scss";
+import { useEffect, useState } from "react";
 
 export const Console = () => {
+  const [mobile, setMobile] = useState(false);
+
+  useEffect(() => {
+    const handleMobile = () => {
+      setMobile(window.matchMedia("(orientation: portrait)").matches);
+    };
+    handleMobile();
+    window.addEventListener("orientationchange", handleMobile);
+    return () => {
+      window.removeEventListener("orientationchange", handleMobile);
+    };
+  });
+
   return (
-    <Container
-      fluid
-      className="console-main-design"
-    >
-      <Col>
-        <Row>
-          <Col className="d-flex justify-content-center" xs={12} md={12}>
-            <Row className=" console-design">
-              <Col className="left-controller" xs={2} md={2}></Col>
-              <Col className=" menu-design" xs={8} md={8}>
-                <Row>
-                  <Col className="display">
-                    <h1>Menu</h1>
-                  </Col>
-                </Row>
-              </Col>
-              <Col className="right-controller" xs={2} md={2}></Col>
-            </Row>
-          </Col>
-        </Row>
-      </Col>
+    <Container fluid className="console-main-design">
+      {mobile ? (
+        <>
+          <div className="rotate-message">
+            <h1>Por favor, gira tu dispositivo</h1>
+            <p>
+              Para una mejor experiencia, gira tu teléfono en posición
+              horizontal.
+            </p>
+          </div>
+        </>
+      ) : (
+        <Col>
+          <Row>
+            <Col className="d-flex justify-content-center" xs={12} md={12}>
+              <Row className=" console-design">
+                <Col className="left-controller" xs={2} md={2}></Col>
+                <Col className=" menu-design" xs={8} md={8}>
+                  <Row>
+                    <Col className="display">
+                      <h1>Menu</h1>
+                    </Col>
+                  </Row>
+                </Col>
+                <Col className="right-controller" xs={2} md={2}></Col>
+              </Row>
+            </Col>
+          </Row>
+        </Col>
+      )}
     </Container>
   );
 };
