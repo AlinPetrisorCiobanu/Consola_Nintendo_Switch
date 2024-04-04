5478bc5fa72e5226ca0246feb1490a7bbbbc0056
Rumanito12 <ady_9697@yahoo.com>
2024-04-04 16:27:12 +0200

    ADD: se añaden todos los botones del joystick derecho.

--- src/pages/Console/Console.jsx
+++ src/pages/Console/Console.jsx
@@ -3,7 +3,7 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowsRotate } from "@fortawesome/free-solid-svg-icons";
+import { faArrowsRotate , faHouse } from "@fortawesome/free-solid-svg-icons";
 import "./Console.scss";
 
 export const Console = () => {
@@ -74,24 +74,40 @@ export const Console = () => {
                   <Row className="buttons-arrows">
                     <Col>
                       <Row>
-                        <Col xs={12} md={12} className="d-flex justify-content-center">
+                        <Col
+                          xs={12}
+                          md={12}
+                          className="d-flex justify-content-center"
+                        >
                           <div className="arrows arrow-up">
-                          <p>1</p>
+                            <p>1</p>
                           </div>
                         </Col>
-                        <Col xs={6} md={6} className="d-flex justify-content-center">
+                        <Col
+                          xs={6}
+                          md={6}
+                          className="d-flex justify-content-center"
+                        >
                           <div className="arrows arrow-left">
-                          <p>2</p>
+                            <p>2</p>
                           </div>
                         </Col>
-                        <Col xs={6} md={6} className="d-flex justify-content-center">
+                        <Col
+                          xs={6}
+                          md={6}
+                          className="d-flex justify-content-center"
+                        >
                           <div className="arrows arrow-right">
-                          <p>3</p>
+                            <p>3</p>
                           </div>
                         </Col>
-                        <Col xs={12} md={12} className="d-flex justify-content-center">
+                        <Col
+                          xs={12}
+                          md={12}
+                          className="d-flex justify-content-center"
+                        >
                           <div className="arrows arrow-down">
-                          <p>4</p>
+                            <p>4</p>
                           </div>
                         </Col>
                       </Row>
@@ -99,8 +115,7 @@ export const Console = () => {
                   </Row>
                   <Row className="d-flex justify-content-end">
                     <Col xs={1} md={1} className="botton-cube">
-                      <div className="circle-cube">
-                      </div>
+                      <div className="circle-cube"></div>
                     </Col>
                     <Col xs={0} md={1}></Col>
                   </Row>
@@ -116,7 +131,72 @@ export const Console = () => {
                     )}
                   </Row>
                 </Col>
-                <Col className="right-controller" xs={2} md={2}></Col>
+                <Col className="right-controller" xs={2} md={2}>
+                  <Row className="d-flex justify-content-start botton-plus">
+                    <Col xs={0} md={1}></Col>
+                    <Col xs={2} md={1} className="botton-plus-hover botton-plus-bar-horizontal"></Col>
+                    <Col xs={1} md={1} className="botton-plus-hover botton-plus-bar-vertical"></Col>
+                  </Row>
+
+                  <Row className="buttons-ABXY">
+                    <Col>
+                      <Row>
+                        <Col
+                          xs={12}
+                          md={12}
+                          className="d-flex justify-content-center"
+                        >
+                          <div className="abxy button-x">
+                            <p>X</p>
+                          </div>
+                        </Col>
+                        <Col
+                          xs={6}
+                          md={6}
+                          className="d-flex justify-content-center"
+                        >
+                          <div className="abxy button-y">
+                            <p>Y</p>
+                          </div>
+                        </Col>
+                        <Col
+                          xs={6}
+                          md={6}
+                          className="d-flex justify-content-center"
+                        >
+                          <div className="abxy button-a">
+                            <p>A</p>
+                          </div>
+                        </Col>
+                        <Col
+                          xs={12}
+                          md={12}
+                          className="d-flex justify-content-center"
+                        >
+                          <div className="abxy button-b">
+                            <p>B</p>
+                          </div>
+                        </Col>
+                      </Row>
+                    </Col>
+                  </Row>
+                  <Row className="d-flex justify-content-center">
+                    <Col className="right-joystick">
+                      <Row>
+                        <Col className="center-right-joystick"></Col>
+                      </Row>
+                    </Col>
+                  </Row>
+                  <Row className="d-flex justify-content-start">
+                    <Col className="right-button-home">
+                      <Row>
+                        <Col className="right-home">
+                          <FontAwesomeIcon className="home" icon={faHouse} />
+                        </Col>
+                      </Row>
+                    </Col>
+                  </Row>
+                </Col>
               </Row>
             </Col>
           </Row>
